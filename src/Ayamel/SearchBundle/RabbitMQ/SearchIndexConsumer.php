edec75297a7f420384cc9181eb5c8e34b2717d5b
Evan Villemez <evan.villemez@americancouncils.org>
2013-08-21 15:40:37 -0400

    WIP: started work on integrating elasticsearch

---
+++ src/Ayamel/SearchBundle/RabbitMQ/SearchIndexConsumer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Ayamel\SearchBundle\RabbitMQ;
+
+use OldSound\RabbitMqBundle\RabbitMq\ConsumerInterface;
+use PhpAmqpLib\Message\AMQPMessage;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Consumes messages via RabbitMQ to rebuild the search index
+ * for Resources.
+ *
+ * @package AyamelSearchBundle
+ * @author Evan Villemez
+ */
+class SearchIndexConsumer implements ConsumerInterface
+{
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Rebuild search index for Resources.
+     *
+     * @param AMQPMessage $msg
+     */
+    public function execute(AMQPMessage $msg)
+    {
+        $body = unserialize($msg->body);
+
+
+
+
+
+        return true;
+    }
+
+}
