95ee82c8ff68d1857044b3e11c70fb8fff2ebc16
Evan Villemez <evillemez@gmail.com>
2013-08-28 17:03:42 -0400

    skipping transcoding tests on travis, it's too expensive

--- util/install_ffmpeg.sh
+++ util/install_ffmpeg.sh
@@ -38,7 +38,15 @@ make install
 make distclean
 
 ## libmp3lame
-sudo apt-get install libmp3lame-dev
+sudo apt-get install nasm
+cd ~/ffmpeg_sources
+wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
+tar xzvf lame-3.99.5.tar.gz
+cd lame-3.99.5
+./configure --prefix="$HOME/ffmpeg_build" --enable-nasm --disable-shared
+make
+make install
+make distclean
 
 ## libopus
 cd ~/ffmpeg_sources
