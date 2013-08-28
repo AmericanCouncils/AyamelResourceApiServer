cf72a409878ab88030fa5d966b892a2077252c19
Evan Villemez <evillemez@gmail.com>
2013-08-28 13:45:24 -0400

    added ffmpeg install script for travis

---
+++ util/install_ffmpeg.sh
@@ -0,0 +1,76 @@
+# custom install script for ffmpeg for use on travis
+
+## dependencies
+sudo apt-get update
+sudo apt-get -y install autoconf automake build-essential git libass-dev libgpac-dev \
+  libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libx11-dev \
+  libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev
+mkdir ~/ffmpeg_sources
+
+## yasm
+cd ~/ffmpeg_sources
+wget http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
+tar xzvf yasm-1.2.0.tar.gz
+cd yasm-1.2.0
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
+make distclean
+. ~/.profile
+
+## x264
+cd ~/ffmpeg_sources
+git clone --depth 1 git://git.videolan.org/x264.git
+cd x264
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
+make
+make install
+make distclean
+
+## fdk-aac
+cd ~/ffmpeg_sources
+git clone --depth 1 git://github.com/mstorsjo/fdk-aac.git
+cd fdk-aac
+autoreconf -fiv
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+make distclean
+
+## libmp3lame
+sudo apt-get install libmp3lame-dev
+
+## libopus
+cd ~/ffmpeg_sources
+wget http://downloads.xiph.org/releases/opus/opus-1.0.3.tar.gz
+tar xzvf opus-1.0.3.tar.gz
+cd opus-1.0.3
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
+make
+make install
+make distclean
+
+## libvpx
+cd ~/ffmpeg_sources
+git clone --depth 1 http://git.chromium.org/webm/libvpx.git
+cd libvpx
+./configure --prefix="$HOME/ffmpeg_build" --disable-examples
+make
+make install
+make clean
+
+## FFMPEG!!
+cd ~/ffmpeg_sources
+git clone --depth 1 git://source.ffmpeg.org/ffmpeg
+cd ffmpeg
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig"
+export PKG_CONFIG_PATH
+./configure --prefix="$HOME/ffmpeg_build" \
+  --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+  --bindir="$HOME/bin" --extra-libs="-ldl" --enable-gpl --enable-libass --enable-libfdk-aac \
+  --enable-libmp3lame --enable-libopus --enable-libtheora --enable-libvorbis --enable-libvpx \
+  --enable-libx264 --enable-nonfree --enable-x11grab
+make
+make install
+make distclean
+hash -r
