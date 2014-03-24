310a0360b2056652c27f26b32d5fe7454739a1a3
Evan Villemez <evan.villemez@americancouncils.org>
2014-03-24 11:47:58 -0400

    renamed index and index_dev.php to app and app_dev.php to match convention, updated common

--- web/app_dev.php
+++ web/app_dev.php
@@ -18,11 +18,13 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\Debug\Debug;
 
-ini_set('display_errors', 'on');
 $__start = microtime(true);
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+Debug::enable();
+
 $apcLoader = new ApcClassLoader('ayamel.autoload', $loader);
 $loader->unregister();
 $apcLoader->register(true);
