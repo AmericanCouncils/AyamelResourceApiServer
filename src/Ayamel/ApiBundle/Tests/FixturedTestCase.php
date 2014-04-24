bc692a5fcca4367fafc27746ae7783dc07d50a87
Evan Villemez <evillemez@gmail.com>
2014-04-23 23:56:29 -0400

    Closes #151 - implemented dates as timestamps, updated web-services-bundle version, updated tests accordingly, removed dead code

--- src/Ayamel/ApiBundle/Tests/FixturedTestCase.php
+++ src/Ayamel/ApiBundle/Tests/FixturedTestCase.php
@@ -6,22 +6,6 @@ use AC\WebServicesBundle\TestCase;
 
 class FixturedTestCase extends TestCase
 {
-    protected $user;
-    public function setUp()
-    {
-        parent::setUp();
-
-        // Do I need to worry about authentication here?
-
-        // $container = $this->getClient()->getContainer();
-        // $doc = $container->get('doctrine_mongodb');
-        // $repo = $doc->getRepository('ACFlagshipBundle:User');
-        // $user = $repo->findOneBy(['email' => 'queenie16@yahoo.com']);
-        // if (empty($user)) {
-        //     throw new \Exception("couldn't find a user to log in as");
-        // }
-        // $this->user = $user;
-    }
     protected function getFixtureClass()
     {
         return 'Ayamel\ApiBundle\Tests\AyamelFixture';
--- src/Ayamel/SearchBundle/Tests/SearchApiTest.php
+++ src/Ayamel/SearchBundle/Tests/SearchApiTest.php
@@ -29,10 +29,10 @@ class SearchApiTest extends FixturedTestCase
         $res = $this->callJsonApi('GET', '/api/v1/resources/search?q=&filter:type=audio', ['expectedCode' => 400]);
     }
 
-    public function testSimpleSearchApi()
+    public function testSearchApi()
     {
         //hit ayamel api
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user');
         $code = $response['response']['code'];
         $this->assertSame(200, $code);
         $this->assertSame(16, $response['result']['query']['total']);
@@ -40,11 +40,11 @@ class SearchApiTest extends FixturedTestCase
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testLimit()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&limit=5');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&limit=5');
         $code = $response['response']['code'];
         $this->assertSame(200, $code);
         $this->assertSame(16, $response['result']['query']['total']);
@@ -52,11 +52,11 @@ class SearchApiTest extends FixturedTestCase
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testSkip()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&skip=15');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&skip=15');
         $code = $response['response']['code'];
         $this->assertSame(200, $code);
         $this->assertSame(16, $response['result']['query']['total']);
@@ -64,19 +64,19 @@ class SearchApiTest extends FixturedTestCase
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
-    public function testSimpleSearchApiHidesUnauthorizedResources($ids)
+    public function testSearchApiHidesUnauthorizedResources($ids)
     {
         //public request
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&limit=50');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&limit=50');
         $this->assertSame(16, $response['result']['query']['total']);
         foreach ($response['result']['hits'] as $hit) {
             $this->assertTrue(empty($hit['resource']['visibility']));
         }
 
         //private test_client + public
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&limit=50&_key=45678isafgd56789asfgdhf4567');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&limit=50&_key=45678isafgd56789asfgdhf4567');
         $this->assertSame(33, $response['result']['query']['total']);
         foreach ($response['result']['hits'] as $hit) {
             if (!empty($hit['resource']['visibility'])) {
@@ -85,7 +85,7 @@ class SearchApiTest extends FixturedTestCase
         }
 
         //private, test_client2 + public
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&limit=50&_key=55678isafgd56789asfgdhf4568');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&limit=50&_key=55678isafgd56789asfgdhf4568');
         $this->assertSame(50, $response['result']['query']['total']);
         foreach ($response['result']['hits'] as $hit) {
             if (!empty($hit['resource']['visibility'])) {
@@ -95,87 +95,87 @@ class SearchApiTest extends FixturedTestCase
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testTypeFilter()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:type=audio');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:type=audio');
         $this->assertSame(4, count($response['result']['hits']));
 
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:type=video');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:type=video');
         $this->assertSame(4, count($response['result']['hits']));
 
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:type=video,audio');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:type=video,audio');
         $this->assertSame(8, count($response['result']['hits']));
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testSubjectDomainsFilter()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:subjectDomains=science');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:subjectDomains=science');
         $this->assertSame(4, count($response['result']['hits']));
 
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:subjectDomains=weather');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:subjectDomains=weather');
         $this->assertSame(4, count($response['result']['hits']));
 
         //OR
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:subjectDomains=science,weather');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:subjectDomains=science,weather');
         $this->assertSame(7, count($response['result']['hits']));
 
         //AND
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:subjectDomains[]=science&filter:subjectDomains[]=weather');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:subjectDomains[]=science&filter:subjectDomains[]=weather');
         $this->assertSame(1, count($response['result']['hits']));
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testFunctionalDomainsFilter()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:functionalDomains=informative');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:functionalDomains=informative');
         $this->assertSame(9, count($response['result']['hits']));
 
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:functionalDomains=presentational');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:functionalDomains=presentational');
         $this->assertSame(7, count($response['result']['hits']));
 
         //OR
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:functionalDomains=presentational,informative');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:functionalDomains=presentational,informative');
         $this->assertSame(13, count($response['result']['hits']));
 
         //AND
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:functionalDomains[]=presentational&filter:functionalDomains[]=informative');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:functionalDomains[]=presentational&filter:functionalDomains[]=informative');
         $this->assertSame(3, count($response['result']['hits']));
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testRegistersFilter()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:registers=formal');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:registers=formal');
         $this->assertSame(5, count($response['result']['hits']));
 
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:registers=intimate');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:registers=intimate');
         $this->assertSame(7, count($response['result']['hits']));
 
         //OR
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:registers=formal,intimate');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:registers=formal,intimate');
         $this->assertSame(11, count($response['result']['hits']));
 
         //AND
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:registers[]=formal&filter:registers[]=intimate');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:registers[]=formal&filter:registers[]=intimate');
         $this->assertSame(1, count($response['result']['hits']));
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testClientFilter()
     {
         $this->markTestSkipped();
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&filter:client=another-test-client');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&filter:client=another-test-client');
         $this->assertSame(3, count($response['result']['hits']));
     }
 
@@ -185,7 +185,7 @@ class SearchApiTest extends FixturedTestCase
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testLanguageFilter()
     {
@@ -193,7 +193,7 @@ class SearchApiTest extends FixturedTestCase
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testMultipleFilters()
     {
@@ -201,7 +201,7 @@ class SearchApiTest extends FixturedTestCase
         //that contains both geography and politics as subject
         //and has contains either formal or intimate registers
         $response = $this->callJsonApi('GET',
-            '/api/v1/resources/search?q=enim'.
+            '/api/v1/resources/search?q=user'.
             '&filter:registers=formal,intimate'.
             '&filter:subjectDomains[]=geography'.
             '&filter:subjectDomains[]=politics'.
@@ -217,17 +217,17 @@ class SearchApiTest extends FixturedTestCase
 
     public function testEmptyFacets()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user');
         $this->assertTrue(isset($response['result']['facets']));
         $this->assertTrue(empty($response['result']['facets']));
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testTypeFacet()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&facet:type');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&facet:type');
         $facet = $response['result']['facets'][0];
         $this->assertSame(5, count($facet['values']));
         $this->assertSame('type', $facet['field']);
@@ -236,7 +236,7 @@ class SearchApiTest extends FixturedTestCase
         $this->assertSame(0, $facet['other']);
 
         //limit size of facet
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&facet:type=2');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&facet:type=2');
         $facet = $response['result']['facets'][0];
         $this->assertSame(2, count($facet['values']));
         $this->assertSame('type', $facet['field']);
@@ -245,61 +245,61 @@ class SearchApiTest extends FixturedTestCase
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testFunctionalDomainsFacet()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&facet:functionalDomains');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&facet:functionalDomains');
         $facet = $response['result']['facets'][0];
         $this->assertSame(3, count($facet['values'])); //default limit
         $this->assertSame('functionalDomains', $facet['field']);
 
         //test limit
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&facet:functionalDomains=2');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&facet:functionalDomains=2');
         $facet = $response['result']['facets'][0];
         $this->assertSame(2, count($facet['values']));
     }
 
     public function testMultipleFacets()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&facet:type&facet:subjectDomains');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&facet:type&facet:subjectDomains');
         $this->assertSame(2, count($response['result']['facets']));
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testSubjectDomainsFacet()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&facet:subjectDomains');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&facet:subjectDomains');
         $facet = $response['result']['facets'][0];
         $this->assertSame(10, count($facet['values'])); //default limit
         $this->assertSame('subjectDomains', $facet['field']);
 
         //test limit
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&facet:subjectDomains=2');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&facet:subjectDomains=2');
         $facet = $response['result']['facets'][0];
         $this->assertSame(2, count($facet['values']));
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testRegistersFacet()
     {
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&facet:registers');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&facet:registers');
         $facet = $response['result']['facets'][0];
         $this->assertSame(5, count($facet['values'])); //default limit
         $this->assertSame('registers', $facet['field']);
 
         //test limit
-        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=enim&facet:registers=2');
+        $response = $this->callJsonApi('GET', '/api/v1/resources/search?q=user&facet:registers=2');
         $facet = $response['result']['facets'][0];
         $this->assertSame(2, count($facet['values']));
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testClientFacet()
     {
@@ -307,7 +307,7 @@ class SearchApiTest extends FixturedTestCase
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testClientUserFacet()
     {
@@ -315,7 +315,7 @@ class SearchApiTest extends FixturedTestCase
     }
 
     /**
-     * @depends testSimpleSearchApi
+     * @depends testSearchApi
      */
     public function testLangaugeFacet()
     {
