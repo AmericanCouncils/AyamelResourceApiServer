7e0d91b0771c41284ca6f02f32da5c2553a6f742
Chris MacDonald <cmac1000@gmail.com>
2014-04-24 11:14:55 -0400

    always return true from search index consumer

--- src/Ayamel/SearchBundle/RabbitMQ/SearchIndexConsumer.php
+++ src/Ayamel/SearchBundle/RabbitMQ/SearchIndexConsumer.php
@@ -33,32 +33,35 @@ class SearchIndexConsumer implements ConsumerInterface
     {
         $body = unserialize($msg->body);
         $batch = $this->container->getParameter('ayamel.search.elastica_resource_provider.batch');
+
         try {
             if (isset($body['id'])) {
-                echo 'INDEXED ONE'.PHP_EOL;
                 $this->container->get('ayamel.search.resource_indexer')->indexResource($body['id']);
+                echo 'INDEXED ONE'.PHP_EOL;
+                $logger->info(sprintf('Indexed [%s]: %s', $body['id'], $message));
             }
 
             if (isset($body['ids'])) {
                 $this->container->get('ayamel.search.resource_indexer')->indexResources($body['ids'], $batch);
                 echo 'INDEXED MULTI'.PHP_EOL;
+                $logger->info(sprintf('Indexed multiple [%s]: %s', $body['ids'], $message));
             }
-
-            return true;
         } catch (IndexException $e) {
             $logger = $this->container->get('logger');
 
             if ($e instanceof BulkIndexException) {
                 foreach ($e->getMessages() as $id => $message) {
-                    $logger->warning(sprintf('Indexing failed [%s]: %s', $id, $message));
+                    $logger->info(sprintf('Indexing skipped [%s]: %s', $id, $message));
                 }
-
-                return true;
+            } else {
+                $logger->info(sprintf('Indexing skipped [%s]: %s', $body['id'], $e->getMessage()));
             }
-
-            $logger->warning(sprintf('Indexing failed [%s]: %s', $body['id'], $e->getMessage()));
-
-            return false;
         }
+
+        // Always return true to drop the message from the queue; whether or not
+        // the indexing succeeded, it will almost certainly turn out the same way
+        // if we try it again. If the resource needs to be modified to be indexed,
+        // then that modification will trigger a reindexing event anyways.
+        return true;
     }
 }
